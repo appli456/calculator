a9c88883eb856bfeb149ed324e02943db6afed76
appli456 <lirunzhi99@hotmail.com>
2016-01-17 14:27:59 +0800

    create project

---
+++ js/calculator.js
@@ -0,0 +1,20 @@
+/**
+ * Created by li_rz on 2016/1/17.
+ */
+var calculator = (function () {
+    'use strict';
+
+    var initModule = function () {
+
+    };
+
+    return {
+        initModule : initModule
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    calculator.initModule();
+});
+
+
